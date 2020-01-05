8f516c4994f21570a5715fcf7a0e93f0ab3aa649
Inaki Villar <inaki.seri@gmail.com>
2020-01-04 19:40:53 -0800

    updating Talaiot in the Pod

--- docker/installer/bin/generator/Versions.kt
+++ docker/installer/bin/generator/Versions.kt
@@ -3,7 +3,7 @@ package com.cdsap.bagan.generator
 
 object Versions {
     const val CHART_EXPERIMENT = "0.1.2"
-    const val POD_INJECTOR_VERSION = "0.1.7"
+    const val POD_INJECTOR_VERSION = "0.1.8"
     const val POD_INJECTOR = "cdsap/bagan-pod-injector:$POD_INJECTOR_VERSION"
     const val PATH = ""
     const val TEMP_FOLDER = "tmp"
