cd0562b34425e13c2568817c2f86d28542a0a738
Inaki Villar <inaki.villar@gotinder.com>
2020-04-30 18:51:05 -0700

    updating 0.1.20

--- frontend/src/com/cdsap/monitor/entities/domain/Pod.kt
+++
@@ -1,9 +0,0 @@
-package com.cdsap.monitor.entities.domain
-
-data class Pod(
-        val name: String,
-        val experiment_id: String,
-        val values: String = "",
-        val configMap: String = "",
-        val iterations: Int
-)
